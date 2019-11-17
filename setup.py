e39c3f20bfb77321c69190da3e106dc72941429b
Alexsandro Camargo <alexx.files@gmail.com>
2019-11-17 03:43:32 -0300

    version 0.1.2 (Change Verson to 0.1.2)

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='netsweeper',
-    version='0.1',
+    version='0.1.2',
     description='Python class to allow programmers to create easily your own net sweeper scripts.',
     author='Alexsandro Camargo',
     py_modules=['netsweeper'],
     author_email='alexx.files@gmail.com',
-    download_url='https://github.com/alexx-files/netsweeper/archive/0.1.0.tar.gz',
+    download_url='https://github.com/alexx-files/netsweeper/archive/0.1.1.tar.gz',
     packages=find_packages(include=['exampleproject', 'exampleproject.*']),
     install_requires=[
         'ping3'
