9c6264f52fa89451092c86510d4848c35c0cbc3b
Alexsandro Camargo <alexx.files@gmail.com>
2019-11-17 04:18:39 -0300

    version 0.1.23 (corrected setup.py descriptions and keywords

--- setup.py
+++ setup.py
@@ -2,16 +2,19 @@ from setuptools import setup, find_packages
 
 setup(
     name='netsweeper',
-    version='0.1.22',
+    version='0.1.23',
     description='Python class to allow programmers to create easily your own net sweeper scripts.',
+    long_description='Python class to allow programmers to create easily your own net sweeper scripts. '
+                     'The examples added to the package already are net sweepers that can be used and improved',
     author='Alexsandro Camargo',
     py_modules=['netsweeper'],
     author_email='alexx.files@gmail.com',
-    download_url='https://github.com/alexx-files/netsweeper/archive/0.1.22.tar.gz',
+    download_url='https://github.com/alexx-files/netsweeper/archive/0.1.23.tar.gz',
     packages=find_packages(include=['exampleproject', 'exampleproject.*']),
     install_requires=[
         'ping3'
     ],
+    keywords='net sweeper netsweeper network scan ping icmp python3',
     python_requires='>=3'
 )
 
