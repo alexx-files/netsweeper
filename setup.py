c962bf7f16627d9ba53b8206d44ef637757b6532
Alexsandro Camargo <alexx.files@gmail.com>
2019-11-17 04:36:16 -0300

    version 0.1.24 (added classifiers and license)

--- setup.py
+++ setup.py
@@ -2,19 +2,27 @@ from setuptools import setup, find_packages
 
 setup(
     name='netsweeper',
-    version='0.1.23',
+    version='0.1.24',
     description='Python class to allow programmers to create easily your own net sweeper scripts.',
     long_description='Python class to allow programmers to create easily your own net sweeper scripts. '
                      'The examples added to the package already are net sweepers that can be used and improved',
     author='Alexsandro Camargo',
     py_modules=['netsweeper'],
     author_email='alexx.files@gmail.com',
-    download_url='https://github.com/alexx-files/netsweeper/archive/0.1.23.tar.gz',
+    download_url='https://github.com/alexx-files/netsweeper/archive/0.1.24.tar.gz',
+    license='MIT',
     packages=find_packages(include=['exampleproject', 'exampleproject.*']),
     install_requires=[
         'ping3'
     ],
     keywords='net sweeper netsweeper network scan ping icmp python3',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Security',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
     python_requires='>=3'
 )
 
